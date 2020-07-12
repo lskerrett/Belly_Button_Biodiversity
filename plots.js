b8cea74c17c9fe622b4c49f48412a30a6d07decc
Laura M. Skerrett <63680712+lskerrett@users.noreply.github.com>
2020-07-12 14:49:13 -0400

    update

--- plots.js
+++ plots.js
@@ -12,12 +12,6 @@ function init() {
     });
 })}
 
-init();
-
-function optionChanged(newSample) {
-  console.log(newSample);
-}
-
 function optionChanged(newSample) {
   buildMetadata(newSample);
   buildCharts(newSample);
@@ -31,6 +25,70 @@ function buildMetadata(sample) {
     var PANEL = d3.select("#sample-metadata");
 
     PANEL.html("");
-    PANEL.append("h6").text(result.location);
+    PANEL.append("h6").text('ID: ' + result.id);
+    PANEL.append("h6").text('ETHNICITY: ' + result.ethnicity);
+    PANEL.append("h6").text('GENDER: ' + result.gender);
+    PANEL.append("h6").text('AGE: ' + result.age);
+    PANEL.append("h6").text('LOCATION: ' + result.location);
+    PANEL.append("h6").text('BBTYPE: ' + result.bbtype);
+    PANEL.append("h6").text('WFREQ: ' + result.wfreq);
+    
+  });
+};
+
+function buildCharts(sample) {
+  d3.json("samples.json").then((data) => {
+      var valuesamples = data.samples;
+      var newarray = valuesamples.filter(sampledata => sampledata.id == sample);
+      var result = newarray[0];
+// Build the bar graph 
+      var otu_ids = result.otu_ids;
+      var otu_labels = result.otu_labels;
+      var samplevalues = result.sample_values;
+      var Filterdata = otu_ids.slice(0, 10).map(otuID => `OTU${otuID}`).reverse();
+      var tracedata = {
+          x: samplevalues.slice(0, 10).reverse(),
+          y: Filterdata,
+          text: otu_labels.slice(0, 10).reverse(),
+          orientation: "h",
+          type: "bar",
+          marker: {
+          color: "Green",
+          }
+      };
+      var data = [tracedata];
+      var layout = {
+          title: "<b>Bacterial Species per Sample </b> <br> Top 10 bacterial species (OTUs)",
+          margin: { t: 50, l: 150 },
+      };
+      Plotly.newPlot("bar", data, layout);
+//Build bubble charts 
+      var tracesample = {
+          x: otu_ids,
+          y: samplevalues,
+          text: otu_labels,
+          type: "scatter",
+          mode: 'markers',
+          marker: {
+              color: otu_ids,
+              colorscale: 'Greens',
+              opacity: 0.8,
+              size: samplevalues,
+              sizemode: 'diameter'
+          }
+      };
+      var data = [tracesample];
+      var chartlayout = {
+          title: "<b> All the Bacterial Species per Sample",
+          xaxis: { title: "OTU ID" },
+          showlegend: false,
+          height: 600,
+          width: 1200
+      };
+      Plotly.newPlot("bubble", data, chartlayout);
   });
 }
+
+init();
+
+
