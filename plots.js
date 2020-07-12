34c72ae0b59a95e8112479ed94cb8fe88f0a49d3
Laura M. Skerrett <63680712+lskerrett@users.noreply.github.com>
2020-07-12 15:36:25 -0400

    final

--- plots.js
+++ plots.js
@@ -15,6 +15,7 @@ function init() {
 function optionChanged(newSample) {
   buildMetadata(newSample);
   buildCharts(newSample);
+  buildGauge(newSample);
 }
 
 function buildMetadata(sample) {
@@ -53,7 +54,7 @@ function buildCharts(sample) {
           orientation: "h",
           type: "bar",
           marker: {
-          color: "Green",
+          color: "rgb(255, 0, 230)",
           }
       };
       var data = [tracedata];
@@ -71,7 +72,7 @@ function buildCharts(sample) {
           mode: 'markers',
           marker: {
               color: otu_ids,
-              colorscale: 'Greens',
+              colorscale: ['rgb(204, 0, 184)', 'rgb(214, 0, 193)','rgb(255, 26, 232)', 'rgb(255, 82, 238)'],
               opacity: 0.8,
               size: samplevalues,
               sizemode: 'diameter'
@@ -89,6 +90,95 @@ function buildCharts(sample) {
   });
 }
 
+//timeseries graph - Gauge Graph
+
+function buildGauge(wfreq) {
+  //frequencey between 0 and 180
+  var level = parseFloat(wfreq) * 20;
+
+  //math calculations for the meter point using MathPI
+  var degrees = 180 - level;
+  var radius = 0.5;
+  var radians = (degrees * Math.PI)/180;
+  var x = radius * Math.cos(radians);
+  var y = radius * Math.sin(radians);
+
+  // creating main path
+  var mainPath = "M -.0 -0.05 L .0 0.05 L";
+  var paX = String(x);
+  var space = " ";
+  var paY = String(y);
+  var pathEnd = "Z";
+  var path = mainPath.concat(paX, space, paY, pathEnd);
+
+  var newdata = [
+      {
+          type: "scatter",
+          x: [0],
+          y: [0],
+          marker: {size:11, color: "85000"},
+          showlegend: false,
+          name: "Freq",
+          text: level,
+          hoverinfo: "text+name"
+      },
+      {
+          values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+          rotation: 90, 
+          text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+          textinfo: "text",
+          textposition: "inside",
+          marker: {
+              //Specify the colors for this graph 
+              colors: [
+                  "rgb(26, 0, 23)",
+                  "rgb(56, 0, 50))",
+                  "rgb(122, 0, 110)",
+                  "rgb(158, 0, 142)",
+                  "rgb(179, 0, 161)",
+                  "rgb(204, 0, 184))",
+                  "rgb(214, 0, 193)",
+                  "rgb(255, 26, 232)",
+                  "rgb(255, 82, 238)",
+                  "rgb(255, 255, 255)",
+              ]
+          },
+      labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+      hoverinfo: "label",
+      hole: 0.5,
+      type: "pie",
+      showlegend: false
+      },
+
+  ];
+  var layout = {
+      shapes: [
+          {
+              type: "path",
+              path: path,
+              fillcolor: "850000",
+              line: {
+                  color: "850000"
+              }
+          }
+      ],
+      title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
+      height: 500,
+      width: 500,
+      xaxis: {
+          zeroline: false,
+          showticklabels: false,
+          showgrid: false,
+          range: [-1,1]
+      },
+      yaxis: {
+          zeroline: false,
+          showticklabels: false,
+          showgrid: false,
+          range: [-1,1]
+      }
+  };
+  var GaugE = document.getElementById("gauge");
+  Plotly.newPlot(GaugE,newdata,layout);
+}
 init();
-
-
